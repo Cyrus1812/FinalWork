74fe506b3d3aaf22bd27c228b32cbb6b8cfc8ce3
MAX <maksimdemidovru@mail.ru>
2022-10-03 01:55:09 +0300

    add Program

---
+++ Program/Program.cs
@@ -0,0 +1,25 @@
+﻿string[] arrayFirst = { "*-*", "Hello Rustem", "-_-", "2022", "superhero" };
+Random random = new Random();
+Printmassive2(arrayFirst);
+Printmassive2(GetSecondarray(arrayFirst));
+string[] GetSecondarray(string[] array)
+{
+    int LengthArray = random.Next(0, 4);
+    int randomIndex;
+   string[] array2 = {"","","","",""} ;
+    for (int i = 0; i < LengthArray; i++)
+    {
+        randomIndex = random.Next(0, 5);
+        array2[i] = array[randomIndex];
+    }
+    return array2;
+}
+
+void Printmassive2(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }   
+     Console.WriteLine();
+}
